143ccd2200ce884d5a8dc762cc1bb7288f058f31
kangtae210 <kangtae210@naver.com>
2021-09-19 21:26:41 +0900

    study

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print("hello data")
+
+# 자료구조의 분류
+# 1. 선형구조
+# - 리스트 : 선형 리스트(Linear List) / 연결 리스트(Linked List)
+# - 스택(Stack)
+# - 큐(Queue)
+# - 데크(Deque)
+
+# 2. 비선형 구조
+# - 트리(Tree)
+# - 그래프(Graph)
+
